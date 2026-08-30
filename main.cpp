fe21e97bb89a8be03b3cbe68be5252a1cd5bb5e4
Maksym Telychko <maksym.telychko@gmail.com>
2026-08-29 19:04:37 -0700

    pack

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "pack.h"
 
 int main()
 {
--- pack.cpp
+++ pack.cpp
@@ -2,3 +2,60 @@
 
 #include <iostream>
 
+// Metaprogramming template compile-time only feature
+
+//  * a parameter pack
+//      * template parameter pack
+//        A template parameter pack is a template parameter that accepts zero or more template arguments
+//        (non-types, types, or templates).
+//      * function parameter pack
+//        A function parameter pack is a function parameter that accepts zero or more function arguments.
+//
+//  * lambda init-capture pack (since C++20)
+//
+//  * structured binding pack (since C++26)
+//
+//  * A lambda init-capture pack is a lambda capture that introduces an init-capture for each of the elements
+//    in the pack expansion of its initializer. (since C++20)
+//
+//  * A structured binding pack is an identifier in the structured binding declaration that introduces
+//    zero or more structured bindings. (since C++26)
+//
+//  The number of elements of a pack is equal to:
+//      * the number of arguments provided for the parameter pack,
+//        if the pack is a template or function parameter pack,
+//      * the number of elements in the pack expansion of its initializer,
+//        if the pack is a lambda init-capture pack, (since C++20)
+//      * structured binding size of the initializer less the number of non-pack elements
+//        in the structured binding declaration, if the pack is a structured binding pack. (since C++26)
+
+// sizeof... operator
+
+// A non-type template parameter pack with an optional name
+//type ... pack-name (optional)
+
+// A type template parameter pack with an optional name
+//type-parameter-key ... pack-name (optional)
+
+// A constrained type template parameter pack with an optional name (since C++20)
+//type-constraint ... pack-name (optional) (since C++20)
+
+// A template template parameter pack with an optional name
+//template < parameter-list > class ... pack-name (optional) (until C++17)
+//template < parameter-list > type-parameter-key ... pack-name (optional) (since C++17)
+
+// A function parameter pack with an optional name
+//Function parameter pack (a form of declarator, appears in a function parameter list of a variadic function template)
+//pack-name ... pack-param-name (optional)
+
+// Pack expansion: expands to a list of zero or more patterns. The pattern must include at least one pack.
+//Pack expansion (appears in a body of a template)
+//pattern ...
+
+// TODO: template template parameters
+
+// A template with at least one parameter pack is called a variadic template.
+template <class... >
+class Pack
+{
+};
